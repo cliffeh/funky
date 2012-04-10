5ebd0fbc936283b0dc2bec944008cd14cf388bd0
Clifton Snyder <cliff.snyder@gmail.com>
2012-04-10 00:38:49 -0400

    first commit

---
+++ src/main/java/net/thingly/funky/App.java
@@ -0,0 +1,13 @@
+package net.thingly.funky;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
